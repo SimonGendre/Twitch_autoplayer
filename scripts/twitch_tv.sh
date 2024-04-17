819123eec09bcfbd6bff81ab2eea46f610a11dcc
SimonGendre <135613008+SimonGendre@users.noreply.github.com>
2024-04-17 20:53:44 +0200

    updated to newest version
    can now use an external file for streamers. no need to modify the script directly

--- scripts/twitch_tv.sh
+++ scripts/twitch_tv.sh
@@ -1,24 +1,35 @@
 #!/bin/bash
 
+#TODO : find a way to turn off the backlight when no one is streaming
+
 #This script need tmux and streamlink to be installed.
 
 #list of streamers you want to watch.
-#if it's first on the list, it is played in priority
-twitchList=("littlebigwhale" "ultia" "maghla" "colas_bim" "ponce" "antoinedaniel" "pepipin" "gom4rt")
+#if it's first on the list, it is player in priority
+#twitchList=("littlebigwhale" "ultia" "maghla" "jeeltv" "pepipin" "evanandkatelyn" "gom4rt")
+twitchList=()
+filename='./streamers.txt'
+sleepTime=1800
+
+# Read file line by line and store each line in the array
+while IFS= read -r line; do
+    twitchList+=("$line")
+done < "$filename"
 
 #
 #Plays the stream of the streamer passed in parameter
 #start the stream in background using tmux
 start_stream() {
     echo "We're now watching $1 !"
-    tmux new-session -d -s twitch_player_stream "streamlink twitch.tv/$1 720p60,best --player-continuous-http --stream-segment-threads=3 --twitch-low-latency --player 'vlc --fullscreen'"
     CURRENT_STREAMER=$1
+    tmux new-session -d -s twitch_player_stream "streamlink twitch.tv/$1 720p60,best --player-continuous-http --stream-segment-threads=3 --twitch-low-latency --player 'vlc --fullscreen'"
+    
 }
 
 # Function to stop streaming
 # it kills the tmux session
 stop_stream() {
-    echo "Closing Live stream..."
+    echo -e "\033[0;31mClosing Live stream... \033[0m"
     tmux kill-session -t twitch_player_stream
     unset CURRENT_STREAMER
 }
@@ -28,8 +39,13 @@ stop_stream() {
 isLive() {
     a=$(curl -s "https://decapi.me/twitch/uptime/$1" )
     if [[ "$a" == *"offline"* ]]; then
+        if [[ "$CURRENT_STREAMER" == "$1" ]];then
+            
+            unset CURRENT_STREAMER
+        fi
         return 1
     else
+        
         return 0
     fi
 }
@@ -41,29 +57,37 @@ isBeforeCurrent() {
         return 0
     fi
     
-    # Get index of current_streamer
     local current_index=-1
+    local target_index=-1
+    
+    # Find index of CURRENT_STREAMER and target streamer
     for j in "${!twitchList[@]}"; do
         if [[ "${twitchList[$j]}" == "$CURRENT_STREAMER" ]]; then
             current_index=$j
+        fi
+        if [[ "${twitchList[$j]}" == "$1" ]]; then
+            target_index=$j
+        fi
+        # If both indexes are found, no need to continue the loop
+        if [ $current_index -ge 0 ] && [ $target_index -ge 0 ]; then
             break
         fi
     done
     
-    # If streamer ($1) is before current_user in the twitchlist array -> true
-    for j in "${!twitchList[@]}"; do
-        if [[ "${twitchList[$j]}" == "$1" ]]; then
-            # If the index of $1 is less than the index of $CURRENT_STREAMER
-            if [ "$j" -lt "$current_index" ]; then
-                return 0
-            else
-                return 1
-            fi
-        fi
-    done
-    # If $1 is not found in the list, return false
-    return 1
+    # If either index is not found, return false
+    if [ $current_index -lt 0 ] || [ $target_index -lt 0 ]; then
+        return 1
+    fi
+    
+    # Compare indexes to determine priority
+    if [ $target_index -lt $current_index ]; then
+        
+        return 0
+    else
+        return 1
+    fi
 }
+
 
 # Main loop
 while :
@@ -80,6 +104,7 @@ do
             if [ "$streamer" != "$CURRENT_STREAMER" ]; then
                 #check the position of the streamer before showing its magnificent content
                 if isBeforeCurrent $streamer; then
+                    echo -e "\033[0;33m$streamer is before $CURRENT_STREAMER. switching... \033[0m"
                     stop_stream
                     start_stream $streamer
                 fi
@@ -87,9 +112,10 @@ do
             
         else
             #sad_face.jpg
-            echo "$streamer is offline"
+            echo "${streamer} is offline"
         fi
     done
     # Wait before starting the loop again
-    sleep 300
+    echo -e "\033[0;33mSleeping $sleepTime seconds... \033[0m"
+    sleep $sleepTime
 done
